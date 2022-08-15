b1308e05e6913436c56928702d0d7e6348fff1fd
Studio <studio@MacBook-Pro.local>
2022-08-15 15:01:55 +0200

    simple component of registration

--- project-typescript-react/src/App.tsx
+++ project-typescript-react/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Registration from "./components/Registration/Registration";
 import User from "./components/User/UserFetc";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <User></User>
+      {/* <User/> */}
+      <Registration/>
     </div>
   );
 }
